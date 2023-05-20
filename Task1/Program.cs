84a834e6c13246a735d258a6c2ba8f8d83fdee40
Radmir Shagiev <veselday@gmail.com>
2023-05-20 19:03:31 +0500

    возведение "а" в "в"

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Please, write number A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please, write number B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = 1;
+if (b<1)
+Console.Write("The number B must be natural\n");
+else
+{
+while (b!=0)
+{
+c = c * a;
+b = b - 1;
+}
+Console.Write($"The number A in natural degree B is {c}");
+}
